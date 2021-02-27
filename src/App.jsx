1b165217ee3bc6a1505cac6b419aaaa357491dff
Zacharias Erlandsson <zacharias.erlandsson@gmail.com>
2021-02-27 14:30:29 +0100

    Removed boilerplate React code

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import logo from './logo.svg'
-import './App.css'
 import Contact from './contact/Contact.jsx'
 import {
   BrowserRouter as Router,
@@ -11,23 +9,7 @@ import {
 function App() {
   return (
     <Router>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-            >
-            Learn React
-          </a>
-          <Link to='/contact'>Kontakt</Link>
-        </header>
-      </div>
+      <Link to='/contact'>Kontakt</Link>
       <Switch>
         <Route path='/contact'>
           <Contact />
