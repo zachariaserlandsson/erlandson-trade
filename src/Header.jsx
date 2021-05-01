c4b6168ec5deac9870a8cd36e37016f6e298fbba
Zacharias Erlandsson <zacharias.erlandsson@gmail.com>
2021-05-01 11:55:50 +0200

    Removed Home button, can navigate to home by clicking logo header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,12 +1,6 @@
 import React from 'react'
 import { Image, Button } from 'antd'
-import {
-  MailOutlined,
-  HomeOutlined,
-  FireOutlined,
-  TagOutlined,
-  DollarOutlined,
-} from '@ant-design/icons'
+import { MailOutlined, FireOutlined, TagOutlined, DollarOutlined } from '@ant-design/icons'
 import { useHistory } from 'react-router-dom'
 import EtText from './EtText.jsx'
 
@@ -15,24 +9,17 @@ const Header = () => {
   const navigateTo = (url) => () => history.replace(url)
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '1em' }}>
-      {/* <div style={{ borderStyle: 'solid', borderRadius: '15px', padding: '0.5em' }}> */}
       <div>
         <Image
           preview={false}
           width={500}
           src='http://www.erlandsson-trade.se/images/erlandsontrade.jpg'
+          onClick={navigateTo('/')}
+          style={{ cursor: 'pointer' }}
         />
         <div
           style={{ display: 'flex', justifyContent: 'space-between', marginTop: '1em', width: 500 }}
         >
-          <Button
-            type='text'
-            icon={<HomeOutlined />}
-            onClick={navigateTo('/')}
-            style={{ padding: 0 }}
-          >
-            <EtText>Hem</EtText>
-          </Button>
           <Button
             type='text'
             icon={<MailOutlined />}
