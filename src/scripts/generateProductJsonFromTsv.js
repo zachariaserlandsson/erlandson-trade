883a4aff30fc7d346643ff653db812654b7ab91a
Zacharias Erlandsson <zacharias.erlandsson@gmail.com>
2021-08-28 17:24:03 +0200

    Adapted script to new sheet format, added inventory_beta file with new structure

--- src/scripts/generateProductJsonFromTsv.js
+++ src/scripts/generateProductJsonFromTsv.js
@@ -1,38 +1,43 @@
 const fs = require('fs')
 
 const _tabSplitRow = (row) => row.split('\t').map((value) => value.trim())
-const _toCapitalCase = (s) => s.charAt(0).toUpperCase() + s.slice(1).toLowerCase()
-
-const _generateParagraphsObject = (bodyParts) =>
-  bodyParts.reduce((bodyParagraphs, bodyPart) => {
-    bodyPart = bodyPart.trim()
-    if (!bodyPart) return bodyParagraphs
-    const isLinkBodyPart = bodyPart.toLowerCase().includes("se '")
-    return [
-      ...bodyParagraphs,
-      {
-        text: bodyPart.replace(/\[/g, '').replace(/\]/g, ''),
-        ...(isLinkBodyPart ? { href: 'www.google.com' } : {}),
-      },
-    ]
-  }, [])
 
 const _generateProductObject = (row) => {
-  const bodyParts = row[1].replace(/"/g, '').split('  ')
-  const bodyParagraphs = _generateParagraphsObject(bodyParts)
-  return {
-    headline: _toCapitalCase(row[0]),
-    bodyParagraphs,
+  const productData = {
+    rootCategory: row[0],
+    subCategory: row[1],
     articleId: row[2],
-    imageSources: row[3].split(', ').filter(Boolean),
+    imagePath: row[3],
+    headline: row[4],
+    body: row[5],
   }
+  return productData.rootCategory && productData.subCategory ? productData : null
 }
 
 const generateProductJsonFromTsv = (tsvPath) => {
   const sheetRows = fs.readFileSync(tsvPath).toString().split('\n').map(_tabSplitRow)
   const nonTableHeadRows = sheetRows.slice(1)
-  const productObjects = nonTableHeadRows.map(_generateProductObject)
-  console.log(JSON.stringify(productObjects, null, 2))
+  const productObjects = nonTableHeadRows.map(_generateProductObject).filter(Boolean)
+  const structuredProducts = productObjects.reduce((currentObject, product) => {
+    return {
+      ...currentObject,
+      [product.rootCategory]: {
+        [product.subCategory]: [
+          ...((currentObject[product.rootCategory] &&
+            currentObject[product.rootCategory][product.subCategory]) ||
+            []),
+          {
+            articleId: product.articleId,
+            imagePath: product.imagePath,
+            headline: product.headline,
+            body: product.body,
+          },
+        ],
+        ...currentObject[product.rootCategory],
+      },
+    }
+  }, {})
+  console.log(JSON.stringify(structuredProducts, null, 2))
 }
 
 generateProductJsonFromTsv(process.argv[2])
